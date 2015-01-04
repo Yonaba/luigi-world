e6e95ea72fb005892ce48d4085622b3de92c04d9
Yonaba <roland.yonaba@gmail.com>
2015-01-04 09:13:18 +0000

    Tabs to 2spaces

--- src/files/game3.lua
+++ src/files/game3.lua
@@ -43,18 +43,18 @@ player = {
 }
 
 diamond = {
-	x = math.random(600,1000);
-	y = math.random(40,160);
-	width = 9;
-	height = 14;
-	img = diamond
+  x = math.random(600,1000);
+  y = math.random(40,160);
+  width = 9;
+  height = 14;
+  img = diamond
 }
 
 oldpad=Controls.read()
 
 while true do
-	screen:blit(0, 0, background, false)
-	pad = Controls.read()
+  screen:blit(0, 0, background, false)
+  pad = Controls.read()
 
   if pad:cross() and not oldpad:cross() and player.jumpstate == "floor" then 
   player.jumpstate = "jumping" end
@@ -62,69 +62,69 @@ while true do
   if player.jumpstate == "jumping" then 
     player.img = luigi_jump   
     player.jumpspeed = player.jumpspeed - 0.3
-		player.gravity = player.gravity - player.jumpspeed 
+    player.gravity = player.gravity - player.jumpspeed 
   end
    
   if player.gravity < 0 then
-		player.jumpstate = "falling"
+    player.jumpstate = "falling"
   end   
    
-	if player.gravity < 150 and player.jumpstate == "falling" then
-		player.img = luigi_fall
-		player.gravity = player.gravity + (player.jumpspeed + 3)
+  if player.gravity < 150 and player.jumpstate == "falling" then
+    player.img = luigi_fall
+    player.gravity = player.gravity + (player.jumpspeed + 3)
   end
 
-	if player.gravity == 150 then
-		player.jumpspeed = 10
-		player.jumpstate = "floor"
-		player.img = luigi_stand
+  if player.gravity == 150 then
+    player.jumpspeed = 10
+    player.jumpstate = "floor"
+    player.img = luigi_stand
   end
    
-	if player.gravity > 150 then 
-		player.gravity = 150
-	end
+  if player.gravity > 150 then 
+    player.gravity = 150
+  end
 
-	player.y = player.gravity
-	player.walk = player.walk + 1
-	floor.x = floor.x - 1.5
-	if floor.x < - 480 then floor.x = 0 end
-	diamond.x = diamond.x - 3
-	if diamond.x < -10 then diamond.x = math.random(600,1000) end
+  player.y = player.gravity
+  player.walk = player.walk + 1
+  floor.x = floor.x - 1.5
+  if floor.x < - 480 then floor.x = 0 end
+  diamond.x = diamond.x - 3
+  if diamond.x < -10 then diamond.x = math.random(600,1000) end
 
-	--Diamond Collision
-	if (player.x + player.width > diamond.x) and 
-	   (player.x < diamond.x + diamond.width) and 
-		 (player.y + player.height > diamond.y) and 
-		 (player.y < diamond.y + diamond.height) then 
-				player.score = player.score + 3
-				diamond.x = math.random(600,1000)
-				diamond.y = math.random(40,160)
-	end
+  --Diamond Collision
+  if (player.x + player.width > diamond.x) and 
+     (player.x < diamond.x + diamond.width) and 
+     (player.y + player.height > diamond.y) and 
+     (player.y < diamond.y + diamond.height) then 
+        player.score = player.score + 3
+        diamond.x = math.random(600,1000)
+        diamond.y = math.random(40,160)
+  end
 
-	if pad:square() then dofile("index.lua") end
+  if pad:square() then dofile("index.lua") end
 
-	if player.walk >= 1 and player.walk <= 5 and player.jumpstate == "floor" then player.img = luigi_run1
-	elseif player.walk >= 5 and player.walk <= 10 and player.jumpstate == "floor" then player.img = luigi_run2
-	elseif player.walk >= 10 and player.walk <= 15 and player.jumpstate == "floor" then player.img = luigi_run3
-	elseif player.walk >= 15 and player.walk <= 20 and player.jumpstate == "floor" then player.img = luigi_run1
-	elseif player.walk >= 20 and player.walk <= 25 and player.jumpstate == "floor" then player.img = luigi_run2   
-	elseif player.walk >= 25 and player.walk <= 30 and player.jumpstate == "floor" then player.img =luigi_run3
-	elseif player.walk >= 30 and player.walk <= 45 and player.jumpstate == "floor" then player.img = luigi_run1
-	end
-	
-	if player.walk > 35 then player.walk = 1 end
+  if player.walk >= 1 and player.walk <= 5 and player.jumpstate == "floor" then player.img = luigi_run1
+  elseif player.walk >= 5 and player.walk <= 10 and player.jumpstate == "floor" then player.img = luigi_run2
+  elseif player.walk >= 10 and player.walk <= 15 and player.jumpstate == "floor" then player.img = luigi_run3
+  elseif player.walk >= 15 and player.walk <= 20 and player.jumpstate == "floor" then player.img = luigi_run1
+  elseif player.walk >= 20 and player.walk <= 25 and player.jumpstate == "floor" then player.img = luigi_run2   
+  elseif player.walk >= 25 and player.walk <= 30 and player.jumpstate == "floor" then player.img =luigi_run3
+  elseif player.walk >= 30 and player.walk <= 45 and player.jumpstate == "floor" then player.img = luigi_run1
+  end
+  
+  if player.walk > 35 then player.walk = 1 end
 
-	screen:fontPrint(agency,100,262,"Press Square to exit The Level",yellow)
-	screen:blit(floor.x,floor.y + 32,floor.img)
-	screen:blit(floor.x + 480,floor.y + 32,floor.img)
-	screen:blit(player.x,player.y,player.img)
-	screen:blit(diamond.x, diamond.y, diamond.img)
-	
-	--Score printing
-	screen:fontPrint(agency,200,20,""..player.score,blue)
+  screen:fontPrint(agency,100,262,"Press Square to exit The Level",yellow)
+  screen:blit(floor.x,floor.y + 32,floor.img)
+  screen:blit(floor.x + 480,floor.y + 32,floor.img)
+  screen:blit(player.x,player.y,player.img)
+  screen:blit(diamond.x, diamond.y, diamond.img)
+  
+  --Score printing
+  screen:fontPrint(agency,200,20,""..player.score,blue)
 
-	oldpad=pad
-	screen.waitVblankStart()
-	screen.flip()
+  oldpad=pad
+  screen.waitVblankStart()
+  screen.flip()
 end
 
